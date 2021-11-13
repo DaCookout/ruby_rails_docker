4897f192bfdf7b459635d2f9efa83d7e8199744a
Ryan Perry <rperry2174@gmail.com>
2021-11-13 15:36:20 -0800

    Add git author tag

--- config/initializers/pyroscope_initializer.rb
+++ config/initializers/pyroscope_initializer.rb
@@ -3,5 +3,11 @@ require "pyroscope"
 Pyroscope.configure do |config|
   config.app_name = "abalone.ruby.server" # replace this with some name for your application
   config.server_address = ENV["IS_DOCKER"] ? "http://pyroscope-server:4040" : "http://localhost:4040"
-  config.tags = { "git_tag" => ENV["GIT_TAG"] } if ENV["GIT_TAG"]
+
+  tags = {
+    "git_tag":  ENV["GIT_TAG"] || "none",
+    "git_author": ENV["GIT_AUTHOR"] || "none"
+  }
+
+  config.tags = tags
 end
