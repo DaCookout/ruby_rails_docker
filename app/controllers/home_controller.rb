e971fc74ba3d20f419aac41e524874b4ff04cf05
Ryan Perry <rperry2174@gmail.com>
2021-11-14 01:38:26 -0800

    Add slow code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
+    translate_yaml
     return unless current_user
 
     # Retrive current organization stats
@@ -10,4 +11,29 @@ class HomeController < ApplicationController
   end
 
   def show; end
+
+  def encode(msg)
+    msg.to_yaml
+  end
+
+  def decode(str)
+    YAML.safe_load(str)
+  end
+
+  def translate_yaml
+    obj = {
+      name: "Tiffany Mitchell",
+      quantity: 1_000_000,
+      addresses: {
+        address1: "12 Heather Street, Parnell, Auckland, New Zealand",
+        address2: "1 Queen Street, CBD, Auckland, New Zealand"
+      }
+    }
+
+    samples = 25_000
+    samples.times do
+      decode(encode(obj))
+    end
+  end
+  translate_yaml
 end
