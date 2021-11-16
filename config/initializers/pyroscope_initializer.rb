9bfb4a6d3bcb74f074498529d58248f1e426f0a8
Ryan Perry <rperry2174@gmail.com>
2021-11-16 11:15:44 -0800

    Add all non-example changes

--- config/initializers/pyroscope_initializer.rb
+++ config/initializers/pyroscope_initializer.rb
@@ -2,7 +2,7 @@ require "pyroscope"
 
 Pyroscope.configure do |config|
   config.app_name = "abalone.ruby.server" # replace this with some name for your application
-  config.server_address = ENV["IS_DOCKER"] ? "http://pyroscope-server:4040" : "http://localhost:4040"
+  config.server_address = "http://pyroscope-server:4040"
 
   tags = {
     "git_tag": ENV["GIT_TAG"] || "none",
